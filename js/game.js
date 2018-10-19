4a72b00540d2536efc59a34f0396c15ac704dee4
mrabenda92 <exo92mr@gmail.com>
2018-10-19 23:37:41 +0200

    ssome corrections

--- js/game.js
+++ js/game.js
@@ -3,7 +3,6 @@ const cardColors = ["red", "red", "green", "green", "blue", "blue", "brown", "br
 let cards = document.querySelectorAll("div");
 cards = [...cards];
 
-
 const startTime = new Date().getTime();
 
 let activeCard = "";
